c9845ee933c0c244e9d57902ddd0aaad391ebfe3
Diego Linhares <diegomlinhares@gmail.com>
2022-08-03 11:58:31 -0300

    Add application container
    It autoloads all the gems, runs initializers, and manages dependencies

---
+++ system/application.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "bundler/setup"
+require "dry/system/container"
+
+# {Application} is a container that we use it to register dependencies we need
+# to call.
+class Application < Dry::System::Container
+  # Provide environment inferrer.
+  use :env, inferrer: -> { ENV.fetch('RACK_ENV', 'development') }
+
+  # we set "lib" relative to "root" as a path wich contains class definitions
+  # that can be auto-registered.
+  config.auto_register = %w[lib app]
+
+  # this alters $LOAD_PATH hence the "!"
+  load_paths!("lib", "app")
+end
